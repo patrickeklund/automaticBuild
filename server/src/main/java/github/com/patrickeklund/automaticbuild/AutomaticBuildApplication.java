f48097257ab2e3ba6a8d62d1cd09a7ca9de4e583
Patrick Eklund <patrick.eklund@hiq.se>
2017-12-07 14:35:45 +0100

    added server

---
+++ server/src/main/java/github/com/patrickeklund/automaticbuild/AutomaticBuildApplication.java
@@ -0,0 +1,12 @@
+package github.com.patrickeklund.automaticbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticBuildApplication.class, args);
+    }
+}
